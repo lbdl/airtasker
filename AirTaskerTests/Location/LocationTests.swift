531f38851c1db41155e9214fb9e09e5ce7671763
Tim Storey <timstorey@bite-software.com>
2018-01-26 14:35:28 +0700

    Added Quick and Nimble spec tests to project

---
+++ AirTaskerTests/Location/LocationTests.swift
@@ -0,0 +1,21 @@
+//
+//  LocationTests.swift
+//  AirTasker
+//
+//  Created by Timothy Storey on 25/01/2018.
+//Copyright © 2018 BITE-Software. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class LocationTests: QuickSpec {
+    override func spec() {
+        describe("winter") {
+            it("is coming") {
+                expect(1).notTo(equal(2))
+            }
+        }
+    }
+    public func testDummy() {}
+}
