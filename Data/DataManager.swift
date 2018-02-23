dfa7a2548e17bc3650e693a141106f3562cb950b
Tim Storey <timstorey@bite-software.com>
2018-02-23 10:40:06 +0700

    Data manager protocols and docs

---
+++ Data/DataManager.swift
@@ -0,0 +1,63 @@
+//
+//  DataManager.swift
+//  AirTasker
+//
+//  Created by Timothy Storey on 22/02/2018.
+//  Copyright © 2018 BITE-Software. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+protocol DataController {
+    func fetchLocations()
+    func fetchLocationData(for locationId: String)
+    func fetchAvatarData(for profileId: String) -> UIImage?
+}
+
+// we are only going to use shared sessions
+// in this impelmetation
+enum SessionType {
+    case backgroundSession
+    case sharedSession
+    case emphemeralSession
+}
+
+/// The data manager is intended to be passed between view controllers as a data handler
+/// for remote data access and local data persistence.
+/// It is only implemented for default sessions athough it is intended to be extended
+/// for background tasks.
+///
+/// A view controller should not need to know anything about it other
+/// than to call the methods it needs as defined in the DataController protocol.
+///
+/// Data fetched is stored in CoreData. The views should be updated via a fetched results controller.
+/// - return
+///     - An instance of DataManager
+/// - parameters
+///     - storeManager: an object conforming to the PerssistenceController protocol that handles persisting data
+///     - networkManager: an object conforming to the URLSessionProtocol that fetches data
+///     - configuration: a enumeration defining the tyle of session, in this case default
+class DataManager: NSObject, DataController {
+
+    let persistenceManager: PersistenceController
+    let dataSession: URLSessionProtocol
+    
+    required init(storeManager: PersistenceController, networkManager: URLSessionProtocol, configuration: SessionType = .sharedSession) {
+        persistenceManager = storeManager
+        dataSession = networkManager
+    }
+    
+    func fetchLocations() {
+        //
+    }
+    
+    func fetchLocationData(for locationId: String) {
+        //
+    }
+    
+    func fetchAvatarData(for profileId: String) -> UIImage? {
+        return UIImage()
+    }
+    
+}
---
+++ Data/SessionProtocols.swift
@@ -0,0 +1,28 @@
+//
+//  SessionProtocols.swift
+//  AirTasker
+//
+//  Created by Timothy Storey on 22/02/2018.
+//  Copyright © 2018 BITE-Software. All rights reserved.
+//
+
+import Foundation
+
+protocol URLSessionProtocol {
+    typealias DataTaskResult = (Data?, URLResponse?, Error?) -> Void
+    func dataTask(with request: NSURLRequest, completionHandler: @escaping DataTaskResult) -> URLSessionDataTaskProtocol
+}
+
+protocol URLSessionDataTaskProtocol {
+    func resume()
+}
+
+extension URLSession: URLSessionProtocol {
+    func dataTask(with request: NSURLRequest, completionHandler: @escaping DataTaskResult) -> URLSessionDataTaskProtocol {
+        return dataTask(with: request, completionHandler: completionHandler) as URLSessionDataTaskProtocol
+    }
+}
+
+extension URLSessionDataTask: URLSessionDataTaskProtocol {}
+
+
